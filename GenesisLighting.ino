a6d7b5cef64bc4921baaf4c79f3eafe32f370fc6
David Long <braveesnow@outlook.com>
2020-03-03 08:43:42 -0500

    Implemeted button function

--- GenesisLighting.ino
+++ GenesisLighting.ino
@@ -2,53 +2,63 @@
 #define NUM_LEDS 300
 #define INPIN 2
 
-int mode = 0;
-int count = 0;
+int mode = 1;
+int lastButtonState = 0;
 int hue = 255;
 CRGB leds[NUM_LEDS];
 
 void setup() {
   FastLED.addLeds<NEOPIXEL, INPIN>(leds, NUM_LEDS);
+  pinMode(3, INPUT);
+  Serial.begin(9600);
+  Serial.println("Arduino ready.");
 }
 
 void loop() {
   checkButton();
-  switch(count) {
-    case 0:
+  switch (mode) {
+    case 1:
       noLight();
       break;
 
-    case 1:
+    case 2:
       whiteLight();
       break;
   }
 }
 
+boolean checkButton() {
+  int buttonState = digitalRead(3);
+
+  if (buttonState != lastButtonState) {
+    if (buttonState == HIGH) {
+      if (mode == 4) {
+        mode = 1;
+      } else {
+        mode++;
+      }
+      lastButtonState = buttonState;
+      Serial.print("Mode changed to: ");
+      Serial.println(mode);
+      delay(500);
+      return true;
+    }
+  }
+  else return false;
+}
+
 void noLight() {
-  for(int i = 0; i < NUM_LEDS; i++) {
+  Serial.println("Turning off...");
+  for (int i = 0; i < NUM_LEDS; i++) {
     leds[i] = CRGB::Black;
   }
   FastLED.show();
 }
 
 void whiteLight() {
-for(int i = 0; i < NUM_LEDS; i++) {
+  Serial.println("Turning on white lights...");
+  for (int i = 0; i < NUM_LEDS; i++) {
     leds[i] = CRGB::White;
   }
   FastLED.show();
-}
-
-boolean checkButton() {
-  mode = digitalRead(3);
-
-  if (mode == HIGH) {
-    noLight();
-    if (count == 1) {
-      count = 0;
-    } else {
-      count++;
-    }
-    delay(1000);
-    return true;
-  } else return false;
 }
